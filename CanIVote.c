a6d90ad8f53cc6ae2b7c7e4dd1272567ed25b7f0
HiPiyushh <hipiyushh@gmail.com>
2022-09-11 15:01:21 +0530

    logic added.

--- CanIVote.c
+++ CanIVote.c
@@ -3,7 +3,17 @@
 
 int main(){
     
+    int age;
+    printf("Enter your age\n")
+    scanf("%d", &age)
+    if (age > 18)
+    {
+        printf("You can Vote!")
+    } else
+    {
+        printf("You can't Vote!")
+    }
     
-    
+
     return 0;
 }

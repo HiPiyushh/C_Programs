e63d010dd9134bc8d67f44b300d39fbfbc6b2a8c
HiPiyushh <hipiyushh@gmail.com>
2022-09-15 17:47:01 +0530

    heron'sformula.c
    hey there i am put the herons formula program

---
+++ heronsfomula.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{       float s,a,b,c,area;
+	printf("Enter sides of triange: ");
+	scanf("%f %f %f",&a,&b,&c);
+	s=(a+b+c)/2;
+	area=sqrt(s*(s-a)*(s-b)*(s-c));
+	printf("\nArea of triangle with sides %f, %f and %f units is %f square units",a,b,c,area);
+	
+    return 0;
+
+}         
+
